cf15c8b8b1f39b25a6c7d3f97d8bf62972d1fe76
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-05-22 21:07:55 -0300

    test: application-test.yml

--- src/test/java/facul/project/ab_API/AbApiApplicationTests.java
+++ src/test/java/facul/project/ab_API/AbApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test") // Fazer o teste do application-test.yml
+@ActiveProfiles("test")
 @SpringBootTest
 class AbApiApplicationTests {
 
